91e651478cebfdb1ed3fa82161e99f870f814983
Mark Hiner <hiner@wisc.edu>
2012-07-09 14:40:35 -0500

    Converted SignedShortBuffer to delegator
    The loci.formats.gui.SignedShortBuffer class now fully delegates all
    functionality to ome.scifio.util.SignedShortBuffer.

---
+++ components/scifio/src/loci/formats/gui/SignedShortBuffer.java
@@ -0,0 +1,115 @@
+/*
+ * #%L
+ * OME SCIFIO package for reading and converting scientific file formats.
+ * %%
+ * Copyright (C) 2005 - 2012 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package loci.formats.gui;
+
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferShort;
+
+/**
+ * Legacy delegator class for ome.scifio.util.SignedShortBuffer.
+ *
+ * SignedShortBuffer serves the same purpose as java.awt.image.DataBufferShort;
+ * the only difference is that SignedShortBuffer's getType() method
+ * returns DataBuffer.TYPE_USHORT.
+ * This is a workaround for the fact that java.awt.image.BufferedImage does
+ * not support DataBuffers with type DataBuffer.TYPE_SHORT.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/bio-formats/src/loci/formats/gui/SignedShortBuffer.java">Trac</a>,
+ * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/bio-formats/src/loci/formats/gui/SignedShortBuffer.java;hb=HEAD">Gitweb</a></dd></dl>
+ */
+public class SignedShortBuffer extends DataBuffer {
+
+  // -- Fields --
+  
+  private ome.scifio.util.SignedShortBuffer sb;
+
+  // -- Constructors --
+
+  public SignedShortBuffer(int size) {
+    super(DataBuffer.TYPE_USHORT, size);
+    sb = new ome.scifio.util.SignedShortBuffer(size);
+  }
+
+  public SignedShortBuffer(int size, int numbanks) {
+    super(DataBuffer.TYPE_USHORT, size, numbanks);
+    sb = new ome.scifio.util.SignedShortBuffer(size, numbanks);
+  }
+
+  public SignedShortBuffer(short[] data, int size) {
+    super(DataBuffer.TYPE_USHORT, size);
+    sb = new ome.scifio.util.SignedShortBuffer(data, size);
+  }
+
+  public SignedShortBuffer(short[] data, int size, int offset) {
+    super(DataBuffer.TYPE_USHORT, size, 1, offset);
+    sb = new ome.scifio.util.SignedShortBuffer(data, size, offset);
+  }
+
+  public SignedShortBuffer(short[][] data, int size) {
+    super(DataBuffer.TYPE_USHORT, size, data.length);
+    sb = new ome.scifio.util.SignedShortBuffer(data, size);
+  }
+
+  public SignedShortBuffer(short[][] data, int size, int[] offsets) {
+    super(DataBuffer.TYPE_USHORT, size, data.length, offsets);
+    sb = new ome.scifio.util.SignedShortBuffer(data, size, offsets);
+  }
+
+  // -- DataBuffer API methods --
+
+  /* @see java.awt.image.DataBufferShort#getData() */
+  public short[] getData() {
+    return sb.getData();
+  }
+
+  /* @see java.awt.image.DataBufferShort#getData(int) */
+  public short[] getData(int bank) {
+    return sb.getData(bank);
+  }
+
+  /* @see java.awt.image.DataBufferShort#getElem(int, int) */
+  public int getElem(int bank, int i) {
+    return sb.getElem(bank, i);
+  }
+
+  /* @see java.awt.image.DataBufferShort#setElem(int, int, int) */
+  public void setElem(int bank, int i, int val) {
+    sb.setElem(bank, i, val);
+  }
+
+}
