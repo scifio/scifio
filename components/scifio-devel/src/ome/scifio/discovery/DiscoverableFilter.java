1b26933ee13f1602e8c8e735c4da7f609f5bc0ad
Mark Hiner <hinerm@gmail.com>
2013-02-15 13:16:29 -0600

    Updated scifio-devel copyright years to 2013
    used:
    git grep -l 'Copyright (C) [0-9]\{4\} - [0-9]\{4\}' | xargs sed -i '' -e
    's/\(Copyright (C) [0-9]\{4\} - \)[0-9]\{4\}/\12013/'

--- components/scifio-devel/src/ome/scifio/discovery/DiscoverableFilter.java
+++ components/scifio-devel/src/ome/scifio/discovery/DiscoverableFilter.java
@@ -2,7 +2,7 @@
  * #%L
  * OME SCIFIO package for reading and converting scientific file formats.
  * %%
- * Copyright (C) 2005 - 2012 Open Microscopy Environment:
+ * Copyright (C) 2005 - 2013 Open Microscopy Environment:
  *   - Board of Regents of the University of Wisconsin-Madison
  *   - Glencoe Software, Inc.
  *   - University of Dundee
@@ -59,5 +59,13 @@ public @interface DiscoverableFilter {
   
   Class<?> wrappedClass();
   
-  boolean isDefaultEnabled();
+  /**
+   * Returns true if this Filter should be enabled by default.
+   * <p>
+   * Default value is false.
+   * </p>
+   * 
+   * @return
+   */
+  boolean isDefaultEnabled() default false;
 }
