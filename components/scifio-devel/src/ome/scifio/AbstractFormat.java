c1b4c75987ec6559e513c7278b884cb5170fabbd
Mark Hiner <hinerm@gmail.com>
2013-02-21 14:23:44 -0600

    Format is now set when creating translators
    When a Format creates a Translator, the translator's Format should now
    be set properly.

--- components/scifio-devel/src/ome/scifio/AbstractFormat.java
+++ components/scifio-devel/src/ome/scifio/AbstractFormat.java
@@ -294,6 +294,9 @@ public abstract class AbstractFormat<M extends TypedMetadata, C extends Checker,
     TypedTranslator<S, T> trans = 
         (TypedTranslator<S, T>) getContext().getService(PluginAttributeService.class).
         createInstance(Translator.class, kvPairs, null);
+   
+    if (trans != null)
+      trans.setFormat(this);
     
     return trans;
   }
