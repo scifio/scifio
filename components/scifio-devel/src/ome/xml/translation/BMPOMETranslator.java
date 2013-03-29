de8d6d6a7cf9414ac0e948a3d095fec5d67adf79
Mark Hiner <hinerm@gmail.com>
2013-03-29 14:19:18 -0500

    Converted BMPFormat
    The loci.formats BMPReader has been converted to an ome.scifio.format:
    BMPFormat. The legacy layer now delegates all functionality to SCIFIO.

---
+++ components/scifio-devel/src/ome/xml/translation/BMPOMETranslator.java
@@ -0,0 +1,98 @@
+/*
+ * #%L
+ * OME SCIFIO package for reading and converting scientific file formats.
+ * %%
+ * Copyright (C) 2005 - 2013 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+package ome.xml.translation;
+
+import ome.scifio.MetadataLevel;
+import ome.scifio.formats.BMPFormat;
+import ome.xml.meta.OMEMetadata;
+import ome.xml.model.primitives.PositiveFloat;
+
+import org.scijava.Priority;
+import org.scijava.plugin.Attr;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Translator class from {@link ome.scifio.formats.BMPFormat.Metadata} to
+ * {@link ome.xml.meta.OMEMetadata}
+ * <p>
+ * NB: Plugin priority is set to high to be selected over the base
+ * {@link ome.scifio.Metadata} translator.
+ * </p>
+ * 
+ * @author Mark Hiner
+ */
+@Plugin(type = ToOMETranslator.class, priority = Priority.HIGH_PRIORITY,
+attrs = {
+  @Attr(name = BMPOMETranslator.SOURCE, value = BMPFormat.Metadata.CNAME),
+  @Attr(name = BMPOMETranslator.DEST, value = OMEMetadata.CNAME)
+})
+public class BMPOMETranslator extends ToOMETranslator<BMPFormat.Metadata> {
+  
+  // -- Translator API Methods --
+
+  /*
+   * @see OMETranslator#typedTranslate(ome.scifio.Metadata, ome.scifio.Metadata)
+   */
+  @Override
+  protected void typedTranslate(BMPFormat.Metadata source, OMEMetadata dest) {
+    if (source.getMetadataOptions().getMetadataLevel() != MetadataLevel.MINIMUM) {
+      // resolution is stored as pixels per meter; we want to convert to
+      // microns per pixel
+      
+      int pixelSizeX = (Integer) source.getMetadataValue("X resolution");
+      
+      int pixelSizeY = (Integer) source.getMetadataValue("Y resolution");
+
+      double correctedX = pixelSizeX == 0 ? 0.0 : 1000000.0 / pixelSizeX;
+      double correctedY = pixelSizeY == 0 ? 0.0 : 1000000.0 / pixelSizeY;
+
+      if (correctedX > 0) {
+        dest.getRoot().setPixelsPhysicalSizeX(new PositiveFloat(correctedX), 0);
+      }
+      else {
+        LOGGER.warn("Expected positive value for PhysicalSizeX; got {}",
+          correctedX);
+      }
+      if (correctedY > 0) {
+        dest.getRoot().setPixelsPhysicalSizeY(new PositiveFloat(correctedY), 0);
+      }
+      else {
+        LOGGER.warn("Expected positive value for PhysicalSizeY; got {}",
+          correctedY);
+      }
+    }
+  }
+}
