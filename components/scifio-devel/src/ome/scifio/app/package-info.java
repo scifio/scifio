ac801cdfc628a61a41fcfeb04f64ade0b3b58176
Mark Hiner <hiner@wisc.edu>
2013-04-06 13:51:58 -0500

    Added SCIFIOApp
    Added a scijava-common App implementation for SCIFIO. This plugin is
    discovered by the AppService and provides version information, allowing
    the scijava-common context to query and report the discovered SCIFIO
    version.

---
+++ components/scifio-devel/src/ome/scifio/app/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains the scijava-common app implementation for SCIFIO. Provides SCIFIO
+ * versioning information in the scijava-common context.
+ * 
+ * @author Mark Hiner
+ *
+ */
+package ome.scifio.app;
