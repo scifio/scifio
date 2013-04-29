a464027d38a5904f837c8f3a3d81fdc6fb23986f
Mark Hiner <hinerm@gmail.com>
2013-04-29 13:25:02 -0500

    Added JPEG2000 SCIFIO format
    Converted the loci.formats JPEG2000Reader, Writer and MetadataParser to
    and ome.scifio Format.
    
    Converted supporting codec classes as well.

--- components/scifio/src/loci/formats/in/JPEG2000MetadataParser.java
+++ components/scifio/src/loci/formats/in/JPEG2000MetadataParser.java
@@ -54,7 +54,10 @@ import loci.formats.codec.JPEG2000SegmentMarker;
  * <dl><dt><b>Source code:</b></dt>
  * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/bio-formats/src/loci/formats/in/JPEG2000MetadataParser.java">Trac</a>,
  * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/bio-formats/src/loci/formats/in/JPEG2000MetadataParser.java;hb=HEAD">Gitweb</a></dd></dl>
+ * 
+ * @deprecated see ome.scifio.formats.JPEG2000Format.Parser
  */
+@Deprecated
 public class JPEG2000MetadataParser {
 
   // -- Constants --
--- components/scifio/src/loci/formats/out/JPEG2000Writer.java
+++ components/scifio/src/loci/formats/out/JPEG2000Writer.java
@@ -37,13 +37,11 @@
 package loci.formats.out;
 
 import java.io.IOException;
+
 import loci.formats.FormatException;
-import loci.formats.FormatTools;
-import loci.formats.FormatWriter;
-import loci.formats.codec.CompressionType;
-import loci.formats.codec.JPEG2000Codec;
-import loci.formats.codec.JPEG2000CodecOptions;
-import loci.formats.meta.MetadataRetrieve;
+import loci.formats.SCIFIOFormatWriter;
+import loci.legacy.context.LegacyContext;
+import ome.scifio.formats.JPEG2000Format;
 
 /**
  * JPEG2000Writer is the file format writer for JPEG2000 files.
@@ -51,98 +49,39 @@ import loci.formats.meta.MetadataRetrieve;
  * <dl><dt><b>Source code:</b></dt>
  * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/bio-formats/src/loci/formats/out/JPEG2000Writer.java">Trac</a>,
  * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/bio-formats/src/loci/formats/out/JPEG2000Writer.java;hb=HEAD">Gitweb</a></dd></dl>
+ * 
+ * @deprecated see ome.scifio.formats.JPEG2000Format
  */
-public class JPEG2000Writer extends FormatWriter {
-
-  // -- Fields --
+@Deprecated
+public class JPEG2000Writer extends SCIFIOFormatWriter {
 
   // -- Constructor --
 
   /** Creates a new instance. */
   public JPEG2000Writer() {
     super("JPEG-2000", "jp2");
-    compressionTypes = new String[] {CompressionType.J2K_LOSSY.getCompression(), 
-        CompressionType.J2K.getCompression()};
-    //The default codec options
-    options = JPEG2000CodecOptions.getDefaultOptions();
+    format = LegacyContext.getSCIFIO().format().getFormatFromClass(JPEG2000Format.class);
+    try {
+      writer = format.createWriter() ;
+    }
+    catch (ome.scifio.FormatException e) {
+      LOGGER.warn("Failed to create JPEG2000Format components");
+    }
   }
-
+  
   // -- IFormatWriter API methods --
 
-  /**
+  /*
    * @see loci.formats.IFormatWriter#saveBytes(int, byte[], int, int, int, int)
    */
   public void saveBytes(int no, byte[] buf, int x, int y, int w, int h)
     throws FormatException, IOException
   {
-    checkParams(no, buf, x, y, w, h);
-    
-    /*
-    if (!isFullPlane(x, y, w, h)) {
-      throw new FormatException(
-        "JPEG2000Writer does not yet support saving image tiles.");
+    try {
+      writer.savePlane(getSeries(), no, planeCheck(buf, x, y, w, h), x, y, w, h);
     }
-    */
-    //MetadataRetrieve retrieve = getMetadataRetrieve();
-    //int width = retrieve.getPixelsSizeX(series).getValue().intValue();
-    //int height = retrieve.getPixelsSizeY(series).getValue().intValue();
-   
-    out.write(compressBuffer(no, buf, x, y, w, h));
+    catch (ome.scifio.FormatException e) {
+      throw new FormatException(e);
+    }
   }
-
-  /**
-   * Compresses the buffer.
-   * 
-   * @param no the image index within the current file, starting from 0.
-   * @param buf the byte array that represents the image tile.
-   * @param x the X coordinate of the upper-left corner of the image tile.
-   * @param y the Y coordinate of the upper-left corner of the image tile.
-   * @param w the width (in pixels) of the image tile.
-   * @param h the height (in pixels) of the image tile.
-   * @throws FormatException if one of the parameters is invalid.
-   * @throws IOException if there was a problem writing to the file.
-   */
-  public byte[] compressBuffer(int no, byte[] buf, int x, int y, int w, int h)
-    throws FormatException, IOException
-  {
-    checkParams(no, buf, x, y, w, h);
-    MetadataRetrieve retrieve = getMetadataRetrieve();
-    boolean littleEndian =
-      !retrieve.getPixelsBinDataBigEndian(series, 0).booleanValue();
-    int bytesPerPixel = FormatTools.getBytesPerPixel(
-      FormatTools.pixelTypeFromString(
-      retrieve.getPixelsType(series).toString()));
-    int nChannels = getSamplesPerPixel();
-
-    //To be on the save-side
-    if (options == null) options = JPEG2000CodecOptions.getDefaultOptions();
-    options = new JPEG2000CodecOptions(options);
-    options.width = w;
-    options.height = h;
-    options.channels = nChannels;
-    options.bitsPerSample = bytesPerPixel * 8;
-    options.littleEndian = littleEndian;
-    options.interleaved = interleaved;
-    options.lossless = compression == null || 
-    compression.equals(CompressionType.J2K.getCompression());
-    options.colorModel = getColorModel();
-
-    return new JPEG2000Codec().compress(buf, options);
-  }
-    
-  /**
-   * Overridden to indicate that stacks are not supported. 
-   * @see loci.formats.IFormatWriter#canDoStacks() 
-   */
-  public boolean canDoStacks() { return false; }
-
-  /**
-   * Overridden to return the formats supported by the writer.
-   * @see loci.formats.IFormatWriter#getPixelTypes(String) 
-   */
-  public int[] getPixelTypes(String codec) {
-    return new int[] {FormatTools.INT8, FormatTools.UINT8, FormatTools.INT16,
-      FormatTools.UINT16, FormatTools.INT32, FormatTools.UINT32};
-  }
-
 }
