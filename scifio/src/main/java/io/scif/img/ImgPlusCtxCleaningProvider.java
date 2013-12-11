9007db2581d5c12c367b01692fe217f6e3bbf79b
Mark Hiner <hinerm@gmail.com>
2013-12-11 09:59:19 -0600

    IO utility class: dispose context
    Added a new ImgPlusCtxCleaningProvider. This RefProvider creates
    references to ImgPlus instances. When the ImgPluses are garbage
    collected and its reference dequeued, if there are no remaining live
    ImgPluses, the context used to create this ImgPlus is disposed.
    
    This helps to mitigate contexts that are eternal because they were
    created anonymously, e.g. by creating a new ImgOpener in the IO class.
    
    Thus the IO class now registers its ImgPlus instances with the
    RefManagerService.

---
+++ scifio/src/main/java/io/scif/img/ImgPlusCtxCleaningProvider.java
@@ -0,0 +1,161 @@
+/*
+ * #%L
+ * SCIFIO library for reading and converting scientific file formats.
+ * %%
+ * Copyright (C) 2011 - 2013 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package io.scif.img;
+
+import io.scif.AbstractSCIFIOPlugin;
+import io.scif.refs.CleaningRef;
+import io.scif.refs.RefProvider;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.imglib2.meta.ImgPlus;
+
+import org.scijava.Context;
+import org.scijava.plugin.Plugin;
+
+/**
+ * {@link RefProvider} plugin for generating {@link ImgPlusCtxCleaner}
+ * instances.
+ * 
+ * @author Mark Hiner
+ */
+@Plugin(type = RefProvider.class)
+public class ImgPlusCtxCleaningProvider extends AbstractSCIFIOPlugin implements
+	RefProvider
+{
+
+	// -- RefProvider API --
+
+	@Override
+	public boolean handles(Object referent, Object... params) {
+		boolean matches = ImgPlus.class.isAssignableFrom(referent.getClass());
+		matches = matches && params.length == 1;
+		matches = matches && Context.class.isAssignableFrom(params[0].getClass());
+		return matches;
+	}
+
+	@Override
+	public Reference makeRef(Object referent, ReferenceQueue queue,
+		Object... params)
+	{
+		Reference ref = new ImgPlusCtxCleaner(referent, queue, params);
+		return ref;
+	}
+
+	// -- Provided reference class --
+
+	/**
+	 * {@link PhantomReference} implementation used to dispose a {@link Context}
+	 * when all {@link ImgPlus} references have been garbage collected. Create
+	 * this reference as new ImgPlus instances are generated with an anonymous
+	 * Context. This will ensure that when the ImgPlus instances go out of scope,
+	 * the Context is shut down properly.
+	 * 
+	 * @author Mark Hiner
+	 */
+	public static class ImgPlusCtxCleaner extends PhantomReference<ImgPlus>
+		implements CleaningRef
+	{
+
+		// -- Fields --
+
+		private static Map<Context, Integer> ctxRefs =
+			new HashMap<Context, Integer>();
+
+		private Context ctx;
+
+		// -- Constructor --
+
+		public ImgPlusCtxCleaner(Object imgPlus, ReferenceQueue queue,
+			Object... params)
+		{
+			super((ImgPlus) imgPlus, queue);
+			if (params.length != 1) {
+				throw new IllegalArgumentException(
+					"ImgPlusCleanerRef require 1 parameter: a scijava-common Context");
+			}
+
+			try {
+				ctx = (Context) params[0];
+			}
+			catch (ClassCastException e) {
+				throw new IllegalArgumentException(
+					"ImgPlusCleanerRef: invalid parameter");
+			}
+
+			synchronized (ctx) {
+				Integer count = ctxRefs.get(ctx);
+
+				if (count == null) {
+					// Create a new entry for this context
+					ctxRefs.put(ctx, 1);
+				}
+				else {
+					// Increase the count for this context
+					ctxRefs.put(ctx, ctxRefs.get(ctx) + 1);
+				}
+			}
+		}
+
+		// -- CleaningRef API --
+
+		@Override
+		public void cleanup() {
+			if (ctx == null) return;
+
+			synchronized (ctx) {
+				int count = ctxRefs.get(ctx);
+				// Decrease the count for this context
+				count--;
+				// If the count is now 0, dispose the context
+				if (count == 0) {
+					ctxRefs.remove(ctx);
+					ctx.dispose();
+				}
+				// Otherwise, update the stored count
+				else {
+					ctxRefs.put(ctx, count);
+				}
+			}
+		}
+	}
+
+}
