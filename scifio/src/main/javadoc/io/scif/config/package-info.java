e05c74cd95504c30c70ae8bfcce23f93171f5381
Mark Hiner <hinerm@gmail.com>
2014-01-27 13:01:33 -0600

    Create SCIFIOConfig framework
    The SCIFIOConfig class is intended to be a method-call scoped bag of
    context (specifically, configuration options). By putting all
    configuration options in this class and passing down from one method to
    another, configurable classes can take what information they need and
    behave appropriately. This allows exposing of low-level configuration
    even in high-level API simply by adding method signatures that accept a
    SCIFIOConfig.

---
+++ scifio/src/main/javadoc/io/scif/config/package-info.java
@@ -0,0 +1,58 @@
+/*
+ * #%L
+ * SCIFIO library for reading and converting scientific file formats.
+ * %%
+ * Copyright (C) 2011 - 2014 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+/**
+ * This package provides configuration options for all SCIFIO components. 
+ * Configuration is achieved through use of the 
+ * {@link io.scif.config.SCIFIOConfig} class. Methods in this class are prefixed
+ * based on what component they are modifying. The {@code SCIFIOConfig} then
+ * operates as a single container of state, passed between each consumer, which 
+ * queries the values that apply to them.
+ * <p>
+ * Configuration is scoped within method execution, thus SCIFIOConfig objects
+ * should not be cached, but instead always passed through method signatures.
+ * </p>
+ * <p>
+ * Adding new configuration options requires three steps:
+ * <ul>
+ * <li>Add the appropriate field(s) and setter/accessor methods to 
+ * {@code SCIFIOConfig}.</li>
+ * <li>Modify the class being configured to accept {@code SCIFIOConfig} objects
+ * and query the desired information.</li>
+ * <li>Update any downstream code of the newly configurable class to pass along
+ * {@code SCIFIOConfig} objects.</li>
+ * </ul>
+ * </p>
+ *
+ * @author Mark Hiner
+ */
+package io.scif.config;
