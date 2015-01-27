37f3cc387bd0dc13fa8c9e3e773863421afbc9d6
Mark Hiner <hinerm@gmail.com>
2015-01-27 06:05:33 -0600

    wip

--- src/test/java/io/scif/ReadTest.java
+++ src/test/java/io/scif/ReadTest.java
@@ -2,6 +2,7 @@
 package io.scif;
 
 import io.scif.config.SCIFIOConfig;
+import io.scif.config.SCIFIOConfig.ImgMode;
 import io.scif.services.DatasetIOService;
 
 import java.io.IOException;
@@ -15,7 +16,7 @@ public class ReadTest {
 	public static void main(String... args) throws IOException {
 		Context ctx = new Context();
 		DatasetIOService io = ctx.getService(DatasetIOService.class);
-		SCIFIOConfig cfg = new SCIFIOConfig().imgOpenerSetComputeMinMax(true);
+		SCIFIOConfig cfg = new SCIFIOConfig().imgOpenerSetComputeMinMax(false).imgOpenerSetImgModes(ImgMode.PLANAR);
 		while (true) {
 			io.open("C:\\cygwin64\\home\\Mark\\data\\imagej\\img_0_Cy5_1.tif", cfg);
 //			io.open("C:\\cygwin64\\home\\Mark\\data\\imagej\\tubhiswt4D.ome.tif", cfg);
@@ -27,7 +28,7 @@ public class ReadTest {
 	public void timeTest() throws IOException {
 		Context ctx = new Context();
 		DatasetIOService io = ctx.getService(DatasetIOService.class);
-		SCIFIOConfig cfg = new SCIFIOConfig().imgOpenerSetComputeMinMax(true);
+		SCIFIOConfig cfg = new SCIFIOConfig().imgOpenerSetComputeMinMax(false).imgOpenerSetImgModes(ImgMode.PLANAR);
 		for (int i = 0; i < 5000; i++) {
 			io.open("C:\\cygwin64\\home\\Mark\\data\\imagej\\img_0_Cy5_1.tif", cfg);
 //			io.open("C:\\cygwin64\\home\\Mark\\data\\imagej\\tubhiswt4D.ome.tif", cfg);
