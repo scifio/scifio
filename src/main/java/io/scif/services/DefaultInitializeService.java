7340b9503af491d8f93be3a51aafe3a892f5d85b
Curtis Rueden <ctrueden@wisc.edu>
2016-04-06 18:51:43 -0500

    Happy New Year 2016

--- src/main/java/io/scif/services/DefaultInitializeService.java
+++ src/main/java/io/scif/services/DefaultInitializeService.java
@@ -2,18 +2,18 @@
  * #%L
  * SCIFIO library for reading and converting scientific file formats.
  * %%
- * Copyright (C) 2011 - 2015 Board of Regents of the University of
+ * Copyright (C) 2011 - 2016 Board of Regents of the University of
  * Wisconsin-Madison
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
