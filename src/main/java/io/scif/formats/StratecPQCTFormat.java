d816bd6f60c5fea040650930bc28749d187dca41
rimadoma <rdomander@rvc.ac.uk>
2016-12-06 20:15:03 +0000

    Add Stratec pQCT format
    Thanks to @tjrantal for his help!

---
+++ src/main/java/io/scif/formats/StratecPQCTFormat.java
@@ -0,0 +1,449 @@
+/*-
+ * #%L
+ * SCIFIO library for reading and converting scientific file formats.
+ * %%
+ * Copyright (C) 2011 - 2017 SCIFIO developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package io.scif.formats;
+
+import io.scif.*;
+import io.scif.config.SCIFIOConfig;
+import io.scif.io.RandomAccessInputStream;
+import io.scif.util.FormatTools;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import net.imagej.axis.Axes;
+import net.imagej.axis.DefaultLinearAxis;
+
+import org.scijava.plugin.Plugin;
+
+/**
+ * The file format reader for Stratec pQCT files.
+ * <p>
+ * A pQCT (peripheral Quantitative Computed Tomography) device is used to study
+ * the mechanical properties of bone and muscle. Specifically this reader is for
+ * the reconstructed Iddddddd.Mhh CT-image files (d = digit, h = hex).
+ * </p>
+ * In theory the format supports 3D files, i.e. image stacks but they are very
+ * rare. More often several slices are scanned from the same sample, but they
+ * are saved as different image files. In this case "slice distance" is
+ * meaningful metadata, because it helps to position the separate slice files.
+ * This plugin has NOT been tested with a true Stratec 3D file.
+ *
+ * @author Timo Rantalainen (School of Exercise & Nut. Sci., Deakin University,
+ *         Melbourne 2011)
+ * @author Michael Doube (Royal Veterinary College, London)
+ * @author Richard Domander (Royal Veterinary College, London)
+ */
+@Plugin(type = Format.class, name = "Stratec pQCT")
+public class StratecPQCTFormat extends AbstractFormat {
+
+	/** Bytes in a for a valid pQCT file */
+	public static final int HEADER_SIZE = 1609;
+
+	/** Index of device name in the header */
+	public static final int DEVICE_NAME_INDEX = 1050;
+
+	/**
+	 * Read a Pascal style "short string" from the stream
+	 * <p>
+	 * In a short string the first byte marks the length, and 1..n are the
+	 * characters
+	 * </p>
+	 *
+	 * @param stream The stream at the first byte of the string
+	 * @return The string read from the stream
+	 * @throws IOException If reading fails
+	 */
+	private static String readShortString(final RandomAccessInputStream stream)
+		throws IOException
+	{
+		final byte length = stream.readByte();
+		if (length == 0) {
+			return "";
+		}
+		return stream.readString(length);
+	}
+
+	public static String[] generateSuffixes() {
+		final String[] suffixes = new String[256];
+
+		int count = 0;
+		for (int i = 0; i < 16; i++) {
+			final String hexTens = Integer.toHexString(i);
+			for (int j = 0; j < 16; j++) {
+				final String hexOnes = Integer.toHexString(j);
+				final String suffix = "m" + hexTens + hexOnes;
+				suffixes[count] = suffix;
+				count++;
+			}
+		}
+
+		return suffixes;
+	}
+
+	@Override
+	protected String[] makeSuffixArray() {
+		return generateSuffixes();
+	}
+
+	public static class Checker extends AbstractChecker {
+
+		/** The regexp for valid pQCT filenames (without extension) */
+		// Definition from ij.plugins.HandleExtraFileTypes
+		public static final String NAME_FORMAT = "^[iI]\\d{7}";
+
+		@Override
+		public boolean suffixSufficient() {
+			return false;
+		}
+
+		@Override
+		public boolean isFormat(final RandomAccessInputStream stream)
+			throws IOException
+		{
+			final String fileName = Paths.get(stream.getFileName()).getFileName()
+				.toString();
+			final String mainPart = fileName.substring(0, 8);
+			if (!mainPart.matches(NAME_FORMAT) || stream.length() < HEADER_SIZE) {
+				return false;
+			}
+
+			stream.seek(DEVICE_NAME_INDEX);
+			final String deviceName = readShortString(stream);
+
+			return isDeviceNameValid(deviceName);
+		}
+
+		private boolean isDeviceNameValid(final String deviceName) {
+			final String lowerCase = deviceName.toLowerCase();
+			return lowerCase.length() > 4 && lowerCase.indexOf(".typ", deviceName
+				.length() - 4) >= 0;
+		}
+	}
+
+	public static class Metadata extends AbstractMetadata {
+
+		/** Positions of the four measurement info strings in the header */
+		public static final int[] INFO_INDICES = { 662, 743, 824, 905 };
+		/** The date format in Stratec files */
+		private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(
+			"yyyyMMdd");
+		/** Date used when parsing fails */
+		public static final Date DEFAULT_DATE = new Date(0);
+		/** Unit used in spatial calibration */
+		public static final String UNIT = "mm";
+		/** Height in pixels */
+		private short height;
+		/** Width in pixels */
+		private short width;
+		/**
+		 * Number of slices
+		 * <p>
+		 * NB in theory the format supports 3D files, but they are are exceedingly
+		 * rare. However slice number is still often used in metadata. People take
+		 * individual images of the same object, and mark down slice info as if they
+		 * were in the same stack.
+		 * </p>
+		 */
+		private short slices;
+		/** Position of the first slice (mm) */
+		private double sliceStart;
+		/** Distance between slices (mm) */
+		private double sliceDistance;
+		/** Patient name (surname, forename) */
+		private String patientName;
+		private int patientNumber;
+		private int patientAge;
+		private short patientMeasurementNumber;
+		private Date patientBirthDate;
+		private String patientId;
+		private Date measurementDate;
+		private String measurementInfo;
+		/** Image resolution in mm. Same for X and Y axes */
+		private double resolution;
+		private short topEdge;
+		private short leftEdge;
+		/** Name of device used in imaging */
+		private String deviceName;
+		/** Size of the sample in mm */
+		private double objectSize;
+
+		public String getPatientName() {
+			return patientName;
+		}
+
+		public void setPatientName(final String patientName) {
+			this.patientName = patientName;
+		}
+
+		public long getPatientNumber() {
+			return patientNumber;
+		}
+
+		public void setPatientNumber(final int patientNumber) {
+			this.patientNumber = patientNumber;
+		}
+
+		public int getPatientMeasurementNumber() {
+			return patientMeasurementNumber;
+		}
+
+		public void setPatientMeasurementNumber(
+			final short patientMeasurementNumber)
+		{
+			this.patientMeasurementNumber = patientMeasurementNumber;
+		}
+
+		public Date getPatientBirthDate() {
+			return patientBirthDate;
+		}
+
+		public void setPatientBirthDate(final int patientBirthDate) {
+			this.patientBirthDate = parseDate(patientBirthDate);
+		}
+
+		public Date getMeasurementDate() {
+			return measurementDate;
+		}
+
+		public void setMeasurementDate(final int measurementDate) {
+			this.measurementDate = parseDate(measurementDate);
+		}
+
+		private Date parseDate(final int measurementDate) {
+			try {
+				return DATE_FORMAT.parse(String.valueOf(measurementDate));
+			}
+			catch (ParseException e) {
+				return DEFAULT_DATE;
+			}
+		}
+
+		public double getResolution() {
+			return resolution;
+		}
+
+		public void setResolution(final double resolution) {
+			this.resolution = resolution;
+		}
+
+		public int getLeftEdge() {
+			return leftEdge;
+		}
+
+		public void setLeftEdge(final short leftEdge) {
+			this.leftEdge = leftEdge;
+		}
+
+		public int getTopEdge() {
+			return topEdge;
+		}
+
+		public void setTopEdge(final short topEdge) {
+			this.topEdge = topEdge;
+		}
+
+		public String getMeasurementInfo() {
+			return measurementInfo;
+		}
+
+		public void setMeasurementInfo(final RandomAccessInputStream stream) {
+			StringBuffer info = new StringBuffer(320);
+
+			try {
+				for (int i = 0; i < 4; i++) {
+					stream.seek(INFO_INDICES[i]);
+					final String infoBit = readShortString(stream);
+					if (!infoBit.isEmpty()) {
+						info.append(infoBit).append("\n");
+					}
+				}
+				this.measurementInfo = info.toString();
+			}
+			catch (IOException e) {
+				this.measurementInfo = "";
+			}
+		}
+
+		public String getDeviceName() {
+			return deviceName;
+		}
+
+		public void setDeviceName(final String deviceName) {
+			this.deviceName = deviceName;
+		}
+
+		public String getPatientId() {
+			return patientId;
+		}
+
+		public void setPatientId(final String patientId) {
+			this.patientId = patientId;
+		}
+
+		public double getObjectSize() {
+			return objectSize;
+		}
+
+		public void setObjectSize(final double objectSize) {
+			this.objectSize = objectSize;
+		}
+
+		public void setHeight(final short height) {
+			this.height = height;
+		}
+
+		public void setWidth(final short width) {
+			this.width = width;
+		}
+
+		// TODO Populate calibration metadata when it's supported
+		@Override
+		public void populateImageMetadata() {
+			createImageMetadata(1);
+			final ImageMetadata metadata = get(0);
+			metadata.setLittleEndian(true);
+			metadata.setBitsPerPixel(16);
+			metadata.setPixelType(FormatTools.INT16);
+			metadata.setOrderCertain(true);
+			metadata.setPlanarAxisCount(2);
+			metadata.setAxes(new DefaultLinearAxis(Axes.X, UNIT, resolution),
+				new DefaultLinearAxis(Axes.Y, UNIT, resolution));
+			metadata.setAxisLengths(new long[] { width, height });
+			// TODO add calibration function { -32.768, 0.001 } 1/cm
+		}
+
+		public short getWidth() {
+			return width;
+		}
+
+		public short getHeight() {
+			return height;
+		}
+
+		public int getPatientAge() {
+			return patientAge;
+		}
+
+		public void setPatientAge(final int patientAge) {
+			this.patientAge = patientAge;
+		}
+
+		public void setSlices(final short slices) {
+			this.slices = (short) Math.max(1, slices);
+		}
+
+		public short getSlices() {
+			return slices;
+		}
+
+		public void setSliceStart(final double sliceStart) {
+			this.sliceStart = sliceStart;
+		}
+
+		public void setSliceDistance(final double sliceDistance) {
+			this.sliceDistance = sliceDistance;
+		}
+
+		public double getSliceStart() {
+			return sliceStart;
+		}
+
+		public double getSliceDistance() {
+			return sliceDistance;
+		}
+	}
+
+	public static class Parser extends AbstractParser<Metadata> {
+
+		@Override
+		public void typedParse(final RandomAccessInputStream stream,
+			final Metadata meta, final SCIFIOConfig config) throws IOException,
+			FormatException
+		{
+			config.imgOpenerSetComputeMinMax(true);
+			stream.order(true);
+
+			stream.seek(12);
+			meta.setResolution(stream.readDouble());
+
+			stream.seek(30);
+			meta.setSlices(stream.readShort());
+			meta.setSliceStart(stream.readDouble());
+			meta.setSliceDistance(stream.readDouble());
+
+			stream.seek(318);
+			meta.setObjectSize(stream.readDouble());
+			meta.setMeasurementInfo(stream);
+
+			stream.seek(986);
+			meta.setMeasurementDate(stream.readInt());
+
+			stream.seek(DEVICE_NAME_INDEX);
+			meta.setDeviceName(readShortString(stream));
+
+			stream.seek(1085);
+			meta.setPatientMeasurementNumber(stream.readShort());
+			meta.setPatientNumber(stream.readInt());
+			meta.setPatientBirthDate(stream.readInt());
+			meta.setPatientAge(stream.readInt());
+			meta.setPatientName(readShortString(stream));
+
+			stream.seek(1282);
+			meta.setPatientId(readShortString(stream));
+
+			stream.seek(1525);
+			meta.setLeftEdge(stream.readShort());
+			meta.setTopEdge(stream.readShort());
+			meta.setWidth(stream.readShort());
+			meta.setHeight(stream.readShort());
+		}
+	}
+
+	public static class Reader extends ByteArrayReader<Metadata> {
+
+		@Override
+		protected String[] createDomainArray() {
+			return new String[] { FormatTools.MEDICAL_DOMAIN };
+		}
+
+		@Override
+		public ByteArrayPlane openPlane(final int imageIndex, final long planeIndex,
+			final ByteArrayPlane plane, final long[] planeMin, final long[] planeMax,
+			final SCIFIOConfig config) throws FormatException, IOException
+		{
+			final RandomAccessInputStream stream = getStream();
+			stream.seek(HEADER_SIZE);
+			return readPlane(stream, imageIndex, planeMin, planeMax, plane);
+		}
+	}
+}
---
+++ src/test/java/io/scif/formats/StratecPQCTFormatTest.java
@@ -0,0 +1,342 @@
+/*-
+ * #%L
+ * SCIFIO library for reading and converting scientific file formats.
+ * %%
+ * Copyright (C) 2011 - 2017 SCIFIO developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package io.scif.formats;
+
+import static io.scif.formats.StratecPQCTFormat.*;
+import static org.junit.Assert.*;
+
+import io.scif.ByteArrayPlane;
+import io.scif.ImageMetadata;
+import io.scif.config.SCIFIOConfig;
+import io.scif.io.ByteArrayHandle;
+import io.scif.io.RandomAccessInputStream;
+import io.scif.util.FormatTools;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import net.imagej.axis.Axes;
+import net.imagej.axis.CalibratedAxis;
+import net.imagej.axis.DefaultLinearAxis;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.scijava.Context;
+
+/**
+ * Tests for {@link StratecPQCTFormat}
+ *
+ * @author Richard Domander (Royal Veterinary College, London)
+ */
+public class StratecPQCTFormatTest {
+
+	private static final String validName = Paths.get("/home/path/I1234567.m02")
+		.toString();
+	private static final String validDevice = "device.typ";
+	private static final StratecPQCTFormat format = new StratecPQCTFormat();
+	private static final Context context = new Context();
+	private static final Checker checker = new Checker();
+	private static final Parser parser = new Parser();
+
+	@BeforeClass
+	public static void oneTimeSetup() {
+		format.setContext(context);
+	}
+
+	@AfterClass
+	public static void oneTimeTearDown() {
+		context.dispose();
+	}
+
+	@Test
+	public void testMakeSuffixArray() throws Exception {
+		final String[] suffixes = format.makeSuffixArray();
+
+		for (int i = 0; i < 256; i++) {
+			assertEquals("Malformed suffix", 0, suffixes[i].indexOf('m'));
+			final int hexValue = Integer.parseInt(suffixes[i].substring(1), 16);
+			assertEquals("Missing suffix", i, hexValue);
+		}
+	}
+
+	@Test
+	public void testIsFormatFalseNoDevice() throws Exception {
+		// Check that method doesn't crash with NPE even if there's no device data
+		final ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE);
+		final ByteArrayHandle handle = new ByteArrayHandle(buffer);
+		buffer.position(DEVICE_NAME_INDEX);
+		buffer.put((byte) 1);
+		final RandomAccessInputStream stream = new RandomAccessInputStream(context,
+			handle, validName);
+
+		assertFalse(checker.isFormat(stream));
+	}
+
+	@Test
+	public void testIsFormatFalseShortStream() throws Exception {
+		final ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE - 1);
+		buffer.position(DEVICE_NAME_INDEX);
+		buffer.put((byte) validDevice.length());
+		buffer.put(validDevice.getBytes());
+		final ByteArrayHandle handle = new ByteArrayHandle(buffer);
+		final RandomAccessInputStream stream = new RandomAccessInputStream(context,
+			handle, validName);
+
+		assertFalse(checker.isFormat(stream));
+	}
+
+	@Test
+	public void testIsFormatFalseBadDevice() throws Exception {
+		String device = "device.tyq";
+		final ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE);
+		buffer.position(DEVICE_NAME_INDEX);
+		buffer.put((byte) device.length());
+		buffer.put(device.getBytes());
+		final ByteArrayHandle handle = new ByteArrayHandle(buffer);
+		final RandomAccessInputStream stream = new RandomAccessInputStream(context,
+			handle, validName);
+
+		assertFalse(checker.isFormat(stream));
+	}
+
+	@Test
+	public void testIsFormatFalseBadFilename() throws Exception {
+		final ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE);
+		buffer.position(DEVICE_NAME_INDEX);
+		buffer.put((byte) validDevice.length());
+		buffer.put(validDevice.getBytes());
+		final ByteArrayHandle handle = new ByteArrayHandle(buffer);
+		String badFileName = Paths.get("/home/path/I123456G.m02").toString();
+		final RandomAccessInputStream stream = new RandomAccessInputStream(context,
+			handle, badFileName);
+
+		assertFalse(checker.isFormat(stream));
+	}
+
+	@Test
+	public void testIsFormat() throws Exception {
+		String device = "DevIce.TyP"; // Check that case doesn't matter
+		final ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE);
+		buffer.position(DEVICE_NAME_INDEX);
+		buffer.put((byte) device.length());
+		buffer.put(device.getBytes());
+		final ByteArrayHandle handle = new ByteArrayHandle(buffer);
+		final RandomAccessInputStream stream = new RandomAccessInputStream(context,
+			handle, validName);
+
+		assertTrue(checker.isFormat(stream));
+	}
+
+	@Test
+	public void testSetDateBadInt() throws Exception {
+		final Metadata metadata = (Metadata) format.createMetadata();
+
+		metadata.setPatientBirthDate(0);
+
+		assertEquals(Metadata.DEFAULT_DATE, metadata.getPatientBirthDate());
+	}
+
+	@Test
+	public void testMeasurementInfoEmptyIfException() throws Exception {
+		final Metadata metadata = (Metadata) format.createMetadata();
+		final RandomAccessInputStream stream = new RandomAccessInputStream(
+			new Context(), new byte[0]);
+
+		metadata.setMeasurementInfo(stream);
+
+		assertTrue(metadata.getMeasurementInfo().isEmpty());
+	}
+
+	@Test
+	public void testPopulateImageMetadata() throws Exception {
+		// SETUP
+		final Metadata metadata = (Metadata) format.createMetadata();
+		final short width = 10;
+		metadata.setWidth(width);
+		final short height = 15;
+		metadata.setHeight(height);
+		final short slices = 5;
+		metadata.setSlices(slices);
+		final double resolution = 0.5;
+		metadata.setResolution(resolution);
+		final double distance = 0.1;
+		metadata.setSliceDistance(distance);
+		final CalibratedAxis[] expectedAxes = new CalibratedAxis[] {
+			new DefaultLinearAxis(Axes.X, Metadata.UNIT, resolution),
+			new DefaultLinearAxis(Axes.Y, Metadata.UNIT, resolution) };
+
+		// EXERCISE
+		metadata.populateImageMetadata();
+		final ImageMetadata imgMeta = metadata.get(0);
+		final List<CalibratedAxis> axes = imgMeta.getAxes();
+
+		// VERIFY
+		assertTrue(imgMeta.isLittleEndian());
+		assertTrue(imgMeta.isOrderCertain());
+		assertEquals(16, imgMeta.getBitsPerPixel());
+		assertEquals(FormatTools.INT16, imgMeta.getPixelType());
+		assertEquals(2, imgMeta.getPlanarAxisCount());
+		assertEquals(width, imgMeta.getAxisLength(Axes.X));
+		assertEquals(height, imgMeta.getAxisLength(Axes.Y));
+		assertEquals(expectedAxes.length, axes.size());
+		for (int i = 0; i < expectedAxes.length; i++) {
+			final CalibratedAxis expected = expectedAxes[i];
+			final CalibratedAxis axis = axes.get(i);
+			assertEquals(expected.type(), axis.type());
+			assertEquals(expected.unit(), axis.unit());
+			assertEquals(expected.averageScale(0, 1), axis.averageScale(0, 1), 1e-12);
+		}
+	}
+
+	@Test
+	public void testSlicesAtLeastOne() throws Exception {
+		final Metadata metadata = (Metadata) format.createMetadata();
+		metadata.setSlices((short) 0);
+		assertEquals(1, metadata.getSlices());
+	}
+
+	@Test
+	public void testTypedParse() throws Exception {
+		// SETUP
+		final ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE);
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		final double resolution = 0.1234;
+		final double objectSize = 31337;
+		final List<String> measurementInfo = Arrays.asList("CT-X", "", "Additional",
+			"Info");
+		final String expectedInfo = measurementInfo.stream().filter(s -> !s
+			.isEmpty()).reduce("", (s, t) -> s + t + "\n");
+		final int measurementDate = 20160125;
+		final short measurementNumber = 45;
+		final int patientNumber = 536345;
+		final int patientBirthDate = 19450508;
+		final int patientAge = 43;
+		final String patientName = "Max Mustermann";
+		final String patientId = "MAXM1234";
+		final short leftEdge = 5;
+		final short topEdge = 6;
+		final short width = 250;
+		final short height = 200;
+		final short slices = 3;
+		final double sliceStart = 0.12345;
+		final double sliceDistance = 0.6789;
+		buffer.putDouble(12, resolution);
+		buffer.putShort(30, slices);
+		buffer.putDouble(32, sliceStart);
+		buffer.putDouble(40, sliceDistance);
+		buffer.putDouble(318, objectSize);
+		for (int i = 0; i < 4; i++) {
+			putShortString(Metadata.INFO_INDICES[i], buffer, measurementInfo.get(i));
+		}
+		buffer.putInt(986, measurementDate);
+		putShortString(DEVICE_NAME_INDEX, buffer, validDevice);
+		buffer.putShort(1085, measurementNumber);
+		buffer.putInt(1087, patientNumber);
+		buffer.putInt(1091, patientBirthDate);
+		buffer.putInt(1095, patientAge);
+		putShortString(1099, buffer, patientName);
+		putShortString(1282, buffer, patientId);
+		buffer.putShort(1525, leftEdge);
+		buffer.putShort(1527, topEdge);
+		buffer.putShort(1529, width);
+		buffer.putShort(1531, height);
+		RandomAccessInputStream stream = new RandomAccessInputStream(context, buffer
+			.array());
+		final Metadata metadata = new Metadata();
+		final SCIFIOConfig config = new SCIFIOConfig();
+
+		// EXERCISE
+		parser.typedParse(stream, metadata, config);
+
+		// VERIFY
+		assertEquals(resolution, metadata.getResolution(), 1e-12);
+		assertEquals(objectSize, metadata.getObjectSize(), 1e-12);
+		assertEquals(expectedInfo, metadata.getMeasurementInfo());
+		assertEquals(0, metadata.getMeasurementDate().compareTo(
+			new GregorianCalendar(2016, 0, 25).getTime()));
+		assertEquals(validDevice, metadata.getDeviceName());
+		assertEquals(measurementNumber, metadata.getPatientMeasurementNumber());
+		assertEquals(patientNumber, metadata.getPatientNumber());
+		assertEquals(0, metadata.getPatientBirthDate().compareTo(
+			new GregorianCalendar(1945, 4, 8).getTime()));
+		assertEquals(patientAge, metadata.getPatientAge());
+		assertEquals(patientName, metadata.getPatientName());
+		assertEquals(patientId, metadata.getPatientId());
+		assertEquals(leftEdge, metadata.getLeftEdge());
+		assertEquals(topEdge, metadata.getTopEdge());
+		assertEquals(width, metadata.getWidth());
+		assertEquals(height, metadata.getHeight());
+		assertEquals(slices, metadata.getSlices());
+		assertEquals(sliceStart, metadata.getSliceStart(), 1e-12);
+		assertEquals(sliceDistance, metadata.getSliceDistance(), 1e-12);
+	}
+
+	private void putShortString(final int position, final ByteBuffer buffer,
+		final String string)
+	{
+		buffer.position(position);
+		buffer.put((byte) string.length());
+		buffer.put(string.getBytes());
+	}
+
+	@Test
+	public void testOpenPlane() throws Exception {
+		final short width = 10;
+		final short height = 10;
+		final int planeBytes = width * height * 2;
+		final long[] planeMin = { 0, 0 };
+		final long[] planeMax = { width, height };
+		final ByteArrayPlane plane = new ByteArrayPlane(context);
+		plane.setData(new byte[planeBytes]);
+		final ByteBuffer buffer = ByteBuffer.allocate(
+			StratecPQCTFormat.HEADER_SIZE + planeBytes);
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		buffer.position(1529);
+		buffer.putShort(width);
+		buffer.putShort(height);
+		final RandomAccessInputStream stream = new RandomAccessInputStream(context,
+			buffer.array());
+		final Reader reader = (Reader) format.createReader();
+		reader.setSource(stream);
+
+		// EXECUTE
+		reader.openPlane(0, 0, plane, planeMin, planeMax, new SCIFIOConfig());
+
+		// VERIFY
+		assertEquals(
+			"Position of stream incorrect: should point to the end of the stream",
+			StratecPQCTFormat.HEADER_SIZE + planeBytes, stream.getFilePointer());
+	}
+}
