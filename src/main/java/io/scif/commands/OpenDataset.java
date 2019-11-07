b8b1a1e6bb6fc94feba3e9c65a4e7bb58c980ec9
Jan Eglinger <jan.eglinger@fmi.ch>
2019-11-07 13:40:16 +0100

    Update license headers
    Accomplished using:
        mvn license:update-file-header license:update-project-license

--- src/main/java/io/scif/commands/OpenDataset.java
+++ src/main/java/io/scif/commands/OpenDataset.java
@@ -2,17 +2,17 @@
  * #%L
  * SCIFIO library for reading and converting scientific file formats.
  * %%
- * Copyright (C) 2011 - 2018 SCIFIO developers.
+ * Copyright (C) 2011 - 2019 SCIFIO developers.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
