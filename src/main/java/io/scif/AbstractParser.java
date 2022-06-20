9154930debc3b50adbf8221c09cf0078f319822b
hinerm <hinerm@gmail.com>
2022-06-20 09:21:32 -0500

    AbstractParser: formatting

--- src/main/java/io/scif/AbstractParser.java
+++ src/main/java/io/scif/AbstractParser.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -331,7 +331,7 @@ public abstract class AbstractParser<M extends TypedMetadata> extends
 	/**
 	 * Allows implementations of this class that override
 	 * {@link #parse(Location, Metadata, SCIFIOConfig)} to set the metadata.
-	 * 
+	 *
 	 * @param meta the metadata object for this parser
 	 */
 	protected void setMetaData(final Metadata meta) {
